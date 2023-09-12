5f73dfe54c9b7c0927ef2762244414555cf5ca3b
ArtyomKingmang <kingmangapp@gmail.com>
2023-09-12 19:19:20 +0300

    The instructions are located in the enum "Bytecode"

---
+++ src/vm/asm/Bytecode.java
@@ -0,0 +1,25 @@
+package vm.asm;
+
+public enum Bytecode {
+    HALT("halt", 0),
+    CONST("const", 1),
+    ADD("add", 0),
+    SUB("sub", 0),
+    MUL("mul", 0);
+
+    private final String name;
+    private final int nargs;
+
+    Bytecode(String name, int nargs) {
+        this.name = name;
+        this.nargs = nargs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNargs() {
+        return nargs;
+    }
+}
